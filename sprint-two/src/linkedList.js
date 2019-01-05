fd5ddd87ec6d933cb0a7bc81f44ce7fd1b1d57db
DSaquin <dawnsaq@gmail.com>
2019-01-05 11:58:20 -0800

    linkedList almost complete

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,12 +1,38 @@
+//
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    // create new node
+    let newNode = Node(value);
+    // check to see if list.tail is null
+    if(list.tail === null){
+      // set list.head and list.tail to newNode
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      // set node.next of list.tail to new node
+      list.tail.next = newNode;
+      // and setting list.tail to new node
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    // check to see if list.head is null
+    if (list.head === null) {
+      return undefined;
+    } else {
+      // store value of list.head value
+      let oldVal = list.head.value;
+      // set list.head to node.next
+      list.head = list.head.next;
+      // return value of the old list.head
+      return oldVal;
+    }
+
   };
 
   list.contains = function(target) {
