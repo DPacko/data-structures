d3120d54a8b71fa798e6366de3be848229a6665d
DSaquin <dawnsaq@gmail.com>
2019-01-04 18:31:44 -0800

    completed stack and queue for functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,46 @@
 var Queue = function() {
   var someInstance = {};
-
+  // FIFO
   // Use an object with numeric keys to store values
   var storage = {};
+  // {0: a,
+  //  1: b,
+  //  2: b
+  //  }
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    let keys = Object.keys(storage);
+    if(keys){
+      var max = keys.reduce(function(a, b) {
+        return Math.max(a, b);
+      }, 0);
+      storage[max + 1] = value;
+    } else {
+      storage[0] = value;
+    }
+
   };
 
   someInstance.dequeue = function() {
+    var deletedValue;
+        let keys = Object.keys(storage);
+    if(keys.length){
+      var min = keys.reduce(function(a, b) {
+        return Math.min(a, b);
+      }, 20);
+      deletedValue = storage[min];
+      delete storage[min];
+    } else {
+      return undefined;
+    }
+    return deletedValue;
   };
 
+
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,28 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+    // {0: 'cat',
+     //  1: 'dog',
+     //  2: 'aardvark'
+     // }
+
   // Implement the methods below
   someInstance.push = function(value) {
+    // put in a variable the last key by invoking size
+    let index = someInstance.size();
+    // add key/value
+    storage[index] = value;
   };
 
   someInstance.pop = function() {
+    let index = someInstance.size();
+    var deletedValue = storage[index - 1];
+    delete storage[index - 1];
+    return deletedValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
