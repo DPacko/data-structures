18f30f4f617ac540312a9b63694a93ecbbf8b16e
DSaquin <dawnsaq@gmail.com>
2019-01-05 17:22:46 -0800

    completed tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
+// assuming value is always an integer
+//
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +13,32 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  let childNode = new Tree(value);
+  this.children.push(childNode);
 };
 
 treeMethods.contains = function(target) {
+  // recursion function
+  var searchTree = function(node) {
+    // check own value if it's the target
+    if (node.value === target) {
+      // return true
+      return true;
+    }
+    // if children array is not empty
+    if (node.children.length > 0) {
+      let found = false;
+     // loop through children array
+     for (let i = 0; (!found && i < node.children.length); i++) {
+       // invoke recursion on child element
+       found = searchTree(node.children[i]);
+     }
+     return found;
+    }
+    return false;
+  };
+  // invoke recursion function with 'this' Tree
+  return searchTree(this);
 };
 
 
